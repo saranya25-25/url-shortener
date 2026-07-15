306ad15b61b528edda52f97f379beb0ed718ad72
saranya <saranyanekkanti165@gmail.com>
2026-07-15 11:54:41 +0530

    Implement URL analytics dashboard with dynamic date filtering

--- url-shortener-react/src/hooks/useQuery.js
+++ url-shortener-react/src/hooks/useQuery.js
@@ -1,22 +1,19 @@
-import { useQuery } from "react-query"
-import api from "../api/api"
-
+import { useQuery } from "react-query";
+import api from "../api/api";
 
 export const useFetchMyShortUrls = (token, onError) => {
-    return useQuery("my-shortenurls",
-         async () => {
-            return await api.get(
-                "/api/urls/myurls",
-            {
+    return useQuery(
+        "my-shortenurls",
+        async () => {
+            return await api.get("/api/urls/myurls", {
                 headers: {
                     "Content-Type": "application/json",
                     Accept: "application/json",
                     Authorization: "Bearer " + token,
                 },
-            }
-        );
-    },
-          {
+            });
+        },
+        {
             select: (data) => {
                 const sortedData = data.data.sort(
                     (a, b) => new Date(b.createdDate) - new Date(a.createdDate)
@@ -24,51 +21,42 @@ export const useFetchMyShortUrls = (token, onError) => {
                 return sortedData;
             },
             onError,
-            staleTime: 5000
-          }
-        );
+            staleTime: 5000,
+        }
+    );
 };
 
 export const useFetchTotalClicks = (token, onError) => {
-    return useQuery("url-totalclick",
-         async () => {
+    // Automatically uses the current year
+    const year = new Date().getFullYear();
+    const startDate = `${year}-01-01`;
+    const endDate = `${year}-12-31`;
+
+    return useQuery(
+        "url-totalclick",
+        async () => {
             return await api.get(
-                // "/api/urls/totalClicks?startDate=2024-01-01&endDate=2025-12-31",
-                "/api/urls/totalClicks?startDate=2026-01-01&endDate=2026-12-31",
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-                    Authorization: "Bearer " + token,
-                },
-            }
-        );
-    },
-          {
+                `/api/urls/totalClicks?startDate=${startDate}&endDate=${endDate}`,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json",
+                        Authorization: "Bearer " + token,
+                    },
+                }
+            );
+        },
+        {
             select: (data) => {
-                // data.data =>
-                    //  {
-                    //     "2024-01-01": 120,
-                    //     "2024-01-02": 95,
-                    //     "2024-01-03": 110,
-                    //   };
-                      
                 const convertToArray = Object.keys(data.data).map((key) => ({
                     clickDate: key,
-                    count: data.data[key], // data.data[2024-01-01]
+                    count: data.data[key],
                 }));
-                // Object.keys(data.data) => ["2024-01-01", "2024-01-02", "2024-01-03"]
 
-                // FINAL:
-                //   [
-                //     { clickDate: "2024-01-01", count: 120 },
-                //     { clickDate: "2024-01-02", count: 95 },
-                //     { clickDate: "2024-01-03", count: 110 },
-                //   ]
                 return convertToArray;
             },
             onError,
-            staleTime: 5000
-          }
-        );
+            staleTime: 5000,
+        }
+    );
 };
