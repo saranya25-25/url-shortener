765bc5427129922e886ffc70d1c36ccd7f170032
saranya <saranyanekkanti165@gmail.com>
2026-07-17 19:25:54 +0530

    feat: generate secure alphanumeric otp for password reset

--- src/main/java/com/url/shortner/repository/OtpVerificationRepository.java
+++ src/main/java/com/url/shortner/repository/OtpVerificationRepository.java
@@ -1,14 +1,10 @@
 package com.url.shortner.repository;
-
 import com.url.shortner.models.OtpVerification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-
 public interface OtpVerificationRepository
         extends JpaRepository<OtpVerification, Long> {
-
-
     Optional<OtpVerification> findByEmailAndOtp(
             String email,
             String otp
--- src/main/java/com/url/shortner/utils/OtpGenerator.java
+++ src/main/java/com/url/shortner/utils/OtpGenerator.java
@@ -1,29 +1,15 @@
 package com.url.shortner.utils;
-
 import java.util.Random;
-
 public class OtpGenerator {
-
-
     private static final String CHARACTERS =
             "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-
     public static String generateOtp(){
-
         StringBuilder otp = new StringBuilder();
-
         Random random = new Random();
-
-
         for(int i=0;i<8;i++){
-
             int index = random.nextInt(CHARACTERS.length());
-
             otp.append(CHARACTERS.charAt(index));
-
         }
-
         return otp.toString();
     }
 }
